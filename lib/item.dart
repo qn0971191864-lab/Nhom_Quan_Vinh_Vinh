406f031bd65ab5910a4d422227b2d165c5c4b369
Elzfall <telecom00001111@gmail.com>
2026-09-16 10:45:29 +0700

    them class item va fix loi

---
+++ lib/item.dart
@@ -0,0 +1,17 @@
+class Item {
+  String itemName = "Điện thoại thông minh";
+  double startingPrice = 2000000;
+  String description = "Sản phẩm mới chính hãng";
+
+  // Phương thức thiết lập thông tin vật phẩm đấu giá
+  void setItem(String itemName, double startingPrice, String description) {
+    this.itemName = itemName;
+    this.startingPrice = startingPrice;
+    this.description = description;
+  }
+
+  // Phương thức lấy tên vật phẩm
+  String getItemName() {
+    return itemName;
+  }
+}
